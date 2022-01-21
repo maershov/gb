235c72a584d15fbf5b325f33ae6573d84eb28085
MAErshov <m.ershov97@mail.ru>
2022-01-21 20:26:41 +0300

    *change: no #

--- gb2/main.py
+++ gb2/main.py
@@ -2,66 +2,66 @@
 # Реализовать скрипт проверки типа данных каждого элемента. Использовать функцию type()
 # для проверки типа. Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
 
-# newList = [1, 'hello', True]
-# for nl in newList:
-#     print("type of ", nl, " is: ", type(nl))
+newList = [1, 'hello', True]
+for nl in newList:
+    print("type of ", nl, " is: ", type(nl))
 
 # 2. Для списка реализовать обмен значений соседних элементов, т.е.
 # Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д. При нечетном количестве
 # элементов последний сохранить на своем месте. Для заполнения списка элементов необходимо использовать функцию input().
 
-# n = int(input("Enter number of values"))
-# secList = []
-#
-# for i in range(0, n):
-#     val = input("enter value: ")
-#     secList.append(val)
-#
-# secListLen = len(secList)
-# i = 0
-# while i < secListLen -1:
-#     if i + 1 <= secListLen:
-#         buf = secList[i]
-#         secList[i] = secList[i + 1]
-#         secList[i + 1] = buf
-#     i += 2
-# print(secList)
+n = int(input("Enter number of values"))
+secList = []
+
+for i in range(0, n):
+    val = input("enter value: ")
+    secList.append(val)
+
+secListLen = len(secList)
+i = 0
+while i < secListLen -1:
+    if i + 1 <= secListLen:
+        buf = secList[i]
+        secList[i] = secList[i + 1]
+        secList[i + 1] = buf
+    i += 2
+print(secList)
 
 # 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить к какому времени года
 # относится месяц (зима, весна, лето, осень). Напишите решения через list и через dict.
 
 # через list
-# monthInt = int(input("Enter month number"))
-# seasonList = ['зима', 'зима',
-#               'весна', 'весна', 'весна',
-#               'лето', 'лето', 'лето',
-#               'осень', 'осень', 'осень',
-#               'зима']
-# if monthInt >= 1 and monthInt <= 12:
-#     print("сезон этого месяца: ", seasonList[monthInt - 1])
-# else:
-#     print("такого месяца нет")
+monthInt = int(input("Enter month number"))
+seasonList = ['зима', 'зима',
+              'весна', 'весна', 'весна',
+              'лето', 'лето', 'лето',
+              'осень', 'осень', 'осень',
+              'зима']
+if monthInt >= 1 and monthInt <= 12:
+    print("сезон этого месяца: ", seasonList[monthInt - 1])
+else:
+    print("такого месяца нет")
 
 # через dict
-# monthInt = int(input("Enter month number"))
-# seasonDict = {1: 'зима', 2: 'зима',
-#               3: 'весна', 4: 'весна', 5: 'весна',
-#               6: 'лето', 7: 'лето', 8: 'лето',
-#               9: 'осень', 10: 'осень', 11: 'осень',
-#               12: 'зима'}
-# if monthInt >= 1 and monthInt <= 12:
-#     print("сезон этого месяца: ", seasonDict.get(monthInt))
-# else:
-#     print("такого месяца нет")
+monthInt = int(input("Enter month number"))
+seasonDict = {1: 'зима', 2: 'зима',
+              3: 'весна', 4: 'весна', 5: 'весна',
+              6: 'лето', 7: 'лето', 8: 'лето',
+              9: 'осень', 10: 'осень', 11: 'осень',
+              12: 'зима'}
+if monthInt >= 1 and monthInt <= 12:
+    print("сезон этого месяца: ", seasonDict.get(monthInt))
+else:
+    print("такого месяца нет")
 
 # 4. Пользователь вводит строку из нескольких слов, разделённых пробелами. Вывести каждое слово с новой строки.
 # Строки необходимо пронумеровать. Если в слово длинное, выводить только первые 10 букв в слове.
-# str = input("Enter your string")
-# splittedString = str.split(' ')
-# i = 1
-# for val in splittedString:
-#     print(i, ":", val[:10])
-#     i += 1
+str = input("Enter your string")
+splittedString = str.split(' ')
+i = 1
+for val in splittedString:
+    print(i, ":", val[:10])
+    i += 1
 
 
 # 5. Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел.
